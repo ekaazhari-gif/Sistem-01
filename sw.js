1fc78ab3f5f31f62c27044e18044e989b61ce358
ekaazhari-gif <ekaanessa@moh.gov.my>
2026-09-07 14:04:54 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,42 @@
-const CACHE_NAME = 'pwa-iframe-cache-v3';
+const CACHE_NAME = 'spp-jknps-cache-v3';
 const urlsToCache = [
-    './index.html',
-    './manifest.json',
-    './icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
 ];
 
-// Fasa Pemasangan (Install)
 self.addEventListener('install', event => {
-    // Paksa Service Worker baharu untuk terus aktif tanpa menunggu
-    self.skipWaiting(); 
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Cache dibuka dan fail sedang disimpan');
-                return cache.addAll(urlsToCache);
-            })
-            .catch(err => console.error('Gagal menyimpan cache:', err))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Membuka cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Fasa Pengaktifan (Activate)
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    // Buang cache versi lama
-                    if (cacheName !== CACHE_NAME) {
-                        console.log('Membuang cache lama:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim()) // Mula mengawal halaman serta-merta
-    );
-});
-
-// Fasa Pengambilan (Fetch)
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Kembalikan response dari cache jika dijumpai
-                if (response) {
-                    return response;
-                }
-                // Jika tiada dalam cache, muat dari internet (rangkaian)
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Kembalikan response dari cache jika ada, jika tidak teruskan fetch dari rangkaian
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
